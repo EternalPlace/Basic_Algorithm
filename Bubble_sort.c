4685e5c8c0f15761b4b8c344d398ae85f2c6fbf3
EternalPlace <35691707+EternalPlace@users.noreply.github.com>
2019-01-11 22:53:43 +0800

    Update Bubble_sort.c

--- Bubble_sort.c
+++ Bubble_sort.c
@@ -1,1 +1,10 @@
-A basic C program, bubble sort.
+// 
+for (i=0; i<N-1; i++) {
+    for (j=0; j<N-i-1; j++) {
+        if (a[j]>a[j+1]) {
+            t = a[j];
+            a[j] = a[j+1];
+            a[j+1] = t;
+        }
+    }
+}
